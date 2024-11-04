e217758d37b6fb29fd2c0023ca5af2b78827b057
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-11-04 14:06:47 +0530

    initial commit

---
+++ server/db/db.connect.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://neoGStudent:neoGStudent@neog.nxppkiu.mongodb.net/randomusers?retryWrites=true&w=majority&appName=neoG");
+        console.log("MongoDB connected successfully.");
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
+
+
+//mongodb+srv://neoGStudent:neoGStudent@neog.nxppkiu.mongodb.net/randomusers?retryWrites=true&w=majority&appName=neoG
