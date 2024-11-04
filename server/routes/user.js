f54192caa86ccc8018d7cbd4a2eba0f8f9de8f98
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-11-04 15:57:01 +0530

    fix change

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,32 +5,20 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 10;
-  const searchQuery = req.query.search || ""; // Get the search query
 
   try {
-    // Calculate the number of documents to skip
+    // to calculate the number of documents to skip
     const skip = (page - 1) * pageSize;
 
-    // Modify the query to include search functionality
-    const users = await User.find({
-      $or: [
-        { username: { $regex: searchQuery, $options: "i" } },
-        { email: { $regex: searchQuery, $options: "i" } }
-      ]
-    })
+    const users = await User.find()
       .skip(skip)
       .limit(pageSize);
 
-    const totalCount = await User.countDocuments({
-      $or: [
-        { username: { $regex: searchQuery, $options: "i" } },
-        { email: { $regex: searchQuery, $options: "i" } }
-      ]
-    });
+    const totalCount = await User.countDocuments();
 
     res.json({
       users,
-      totalPages: Math.ceil(totalCount / pageSize),
+      // totalPages: Math.ceil(totalCount / pageSize), 
       currentPage: page
     });
   } catch (error) {
