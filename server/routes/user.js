937c169f2ce6fad400b921fd0841c2ce3a90f89b
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-11-04 16:09:56 +0530

    add search query

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,16 +5,26 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 10;
+  const search = req.query.search || "";
 
   try {
-    // to calculate the number of documents to skip
     const skip = (page - 1) * pageSize;
 
-    const users = await User.find()
+    const users = await User.find({
+      $or: [
+        { username: { $regex: search, $options: 'i' } },
+        { email: { $regex: search, $options: 'i' } }
+      ]
+    })
       .skip(skip)
       .limit(pageSize);
 
-    const totalCount = await User.countDocuments();
+    const totalCount = await User.countDocuments({
+      $or: [
+        { username: { $regex: search, $options: 'i' } },
+        { email: { $regex: search, $options: 'i' } }
+      ]
+    });
 
     res.json({
       users,
